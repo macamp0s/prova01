6d662f8417915cdf837e16dd0eaefbd698ce58b7
Maurício <mauriciocamposdesousa@gmail.com>
2023-05-16 15:57:26 -0300

    aa

--- src/components/Questao01.jsx
+++ src/components/Questao01.jsx
@@ -1,36 +1,43 @@
 const Questao01A = () => {
+
     const alunos = [
         { nome: "sicras", notas: { ap1: 8.4, ap2: 5.4 } },
         { nome: "beltras", notas: { ap1: 6.7, ap2: 3.5 } },
         { nome: "fulas", notas: { ap1: 7.3, ap2: 9.2 } },
-        
+
     ]
 
-    function Questao01B({alunos}) {
+    function Questao01B({ alunos }) {
+        let medias = []
 
-        var myArray=[{}]
-        let id;
+        //usando o foreach para percorrer o array
+        alunos.forEach((aluno) => {
+            //adicionando as medias e o aluno correspondende em um array de objetos com o push
+            const media = (aluno.notas.ap1 + aluno.notas.ap2) / 2;
+            medias.push({ nome: aluno.nome, media: media });
+        });
 
         return (
-            alunos.map(alunos => 
+            //usando o map para percorrer o vetor e imprir o nome de cada aluno e sua media
             <div>
 
-                {myArray=(id= 2, alunos.nome, alunos.notas.ap1 + alunos.notas.ap2)/2}
-                
-                {console.log(myArray)}
-           
-               
-            </div>)
-            )
+                {medias.map((aluno) => (
+                    <div>
+                        {aluno.nome} : {aluno.media}
+                    </div>
+                ))}
+            </div>
+        )
     }
 
-
+   
     return (
         <>
             <Questao01B alunos={alunos}></Questao01B>
-           
-
+            <Filho falarpai={mensagem}></Filho>
         </>
+
+
 
     )
 }
